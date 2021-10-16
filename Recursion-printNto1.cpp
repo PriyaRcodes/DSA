5f900432f22b4a80de5720e51a501921dafa4a31
PriyaRcodes <prir495@gmail.com>
2021-10-16 16:58:13 +0530

    Added print1ToN function and a menu

--- Recursion-printNto1.cpp
+++ Recursion-printNto1.cpp
@@ -12,11 +12,32 @@ void printNto1(int n){
     printNto1(n-1);
 }
 
+void print1toN(int n){
+    if(n==0)
+        return;
+    if(n<0)
+        cout<<"Number must be greater than 0";
+    print1toN(n-1);
+    cout<<n<<" ";
+}
+
 int main()
 {
-    int n;
+    int n, ch;
     cout<<"Enter a number: ";
     cin>>n;
-    printNto1(n);
+    cout<<"Enter your choice from the menu below:\n";
+    cout<<"1. Print 1 to "<<n<<endl;
+    cout<<"2. Print "<<n<<" to 1"<<endl;
+    cin>>ch;
+    switch(ch){
+        case 1:
+            print1toN(n);
+            break;
+        case 2:
+            printNto1(n);
+            break;
+        default: cout<<"Invalid menu choice!";
+    }
     return 0;
 }
